33a9449b67aa582bdddef5559de2a351140f0072
ARAFATH <arafatuddin999@gmail.com>
2023-10-20 04:18:40 -0700

    added to the Mycard

--- src/Components/MyCard.jsx
+++ src/Components/MyCard.jsx
@@ -4,6 +4,8 @@ const MyCard = () => {
     return (
         <div>
             <h2>this is my cart</h2>
+            <h2>this is my cart</h2>
+            <h2>this is my cart</h2>
         </div>
     );
 };
